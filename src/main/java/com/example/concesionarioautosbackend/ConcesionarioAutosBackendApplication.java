0fad6d800508ed0f9c277f89490db556a35fc608
Jhon Zambrano Macias <75482142+JhonZambranoM@users.noreply.github.com>
2023-09-05 19:14:48 -0500

    Segundo Commit
    Prueba de los end-ponits

--- src/main/java/com/example/concesionarioautosbackend/ConcesionarioAutosBackendApplication.java
+++ src/main/java/com/example/concesionarioautosbackend/ConcesionarioAutosBackendApplication.java
@@ -2,12 +2,18 @@ package com.example.concesionarioautosbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ConcesionarioAutosBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConcesionarioAutosBackendApplication.class, args);
+
+    }
+    @GetMapping("/holamundo")
+    public String holamundo(){
+        return "Hola mundo";
     }
 
 }
